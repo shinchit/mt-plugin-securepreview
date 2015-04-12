7b41e39d42556496910ccfce94d193c620d0b73e
shinchit <shinchi.xx@gmail.com>
2015-04-13 05:10:44 +0900

    delete comments

--- plugins/SecurePreview/lib/SecurePreview/Transformer.pm
+++ plugins/SecurePreview/lib/SecurePreview/Transformer.pm
@@ -4,9 +4,6 @@ use strict;
 sub hdlr_template_output_edit_entry {
     my ($cb, $app, $tmpl_str_ref, $param, $tmpl) = @_;
 
-    #my $old = 'mt.cgi';
-    #my $new = 'mt-securepreview.cgi';
-
     my $old = 'preview_entry';
     my $new = 'preview_entry_secure';
     $$tmpl_str_ref =~ s/$old/$new/g;
